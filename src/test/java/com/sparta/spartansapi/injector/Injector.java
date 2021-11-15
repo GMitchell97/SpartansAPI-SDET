23ce785bcd5cf65fc3fffc7e5f1552636becf3c0
Gareth <GMitchell@Spartaglobal.com>
2021-11-15 15:11:53 +0000

    Edited injector to work with call manager

--- src/test/java/com/sparta/spartansapi/injector/Injector.java
+++ src/test/java/com/sparta/spartansapi/injector/Injector.java
@@ -1,6 +1,7 @@
 package com.sparta.spartansapi.injector;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.spartansapi.connection.CallManager;
 import com.sparta.spartansapi.connection.ConnectionManager;
 import com.sparta.spartansapi.dto.SpartanDTO;
 
@@ -11,10 +12,10 @@ public class Injector {
 
     private static final ObjectMapper objectMapper = new ObjectMapper();
 
-    public static SpartanDTO injectSpartanDTO(ConnectionManager connectionManager) {
+    public static SpartanDTO injectSpartanDTO(CallManager callManager) {
         SpartanDTO spartanDTO =  new SpartanDTO();
         try {
-            spartanDTO = objectMapper.readValue(connectionManager.getJSON(), SpartanDTO.class);
+            spartanDTO = objectMapper.readValue(callManager.getJSON(), SpartanDTO.class);
         } catch (IOException e) {
             e.printStackTrace();
         }
