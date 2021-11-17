db3e592da34c3b6443da6c5a3178130e93ee6fd0
swase <fg2g15@soton.ac.uk>
2021-11-17 15:15:52 +0000

    removed view spartan sad path tests

--- src/test/java/com/sparta/spartansapi/connection/CallManager.java
+++ src/test/java/com/sparta/spartansapi/connection/CallManager.java
@@ -10,64 +10,68 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.io.IOException;
 
-public class CallManager {
+public class CallManager{
 
     private HttpResponse<String> httpResponse = null;
 
     public enum Methods {GET, POST, PATCH, DELETE}
 
-    public CallManager(String url) throws IOException, InterruptedException {
+    public CallManager(String url){
         getRequest(url);
     }
 
-    public CallManager(String url, Methods methods, String jsonString) throws IOException, InterruptedException {
+    public CallManager(String url, Methods methods, String jsonString){
         HttpClient httpClient = HttpClient.newHttpClient();
 
         switch (methods) {
-            case GET -> getRequest(url);
+            case GET ->  getRequest(url);
             case POST -> postRequest(url, jsonString);
             case PATCH -> patchRequest(url, jsonString);
             case DELETE -> deleteRequest(url);
         }
     }
 
-    public CallManager(String url, Methods methods) throws IOException, InterruptedException {
+    public CallManager(String url, Methods methods) {
         this(url, methods, "");
     }
 
-    private void getRequest(String url) throws IOException, InterruptedException {
+    private void getRequest(String url) {
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(url)).build();
 
         makeRequest(httpClient, httpRequest);
     }
 
-    private void postRequest(String url, String jsonString) throws IOException, InterruptedException {
+    private void postRequest(String url, String jsonString) {
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(url)).POST(HttpRequest.BodyPublishers.ofString(jsonString)).build();
 
         makeRequest(httpClient, httpRequest);
     }
 
-    private void patchRequest(String url, String jsonString) throws IOException, InterruptedException {
+    private void patchRequest(String url, String jsonString) {
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(url)).PUT(HttpRequest.BodyPublishers.ofString(jsonString)).build();
 
         makeRequest(httpClient, httpRequest);
     }
 
-    private void deleteRequest(String url) throws IOException, InterruptedException {
+    private void deleteRequest(String url) {
         HttpClient httpClient = HttpClient.newHttpClient();
         HttpRequest httpRequest = HttpRequest.newBuilder().uri(URI.create(url)).DELETE().build();
 
         makeRequest(httpClient, httpRequest);
     }
 
-    private void makeRequest(HttpClient httpClient, HttpRequest httpRequest) throws IOException, InterruptedException {
-        httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+    private void makeRequest(HttpClient httpClient, HttpRequest httpRequest) {
+        try {
+            httpResponse = httpClient.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 
-    public String getJson() {
+    public String getJson(){
         return httpResponse.body();
     }
 
--- src/test/resources/features/viewSpartan.feature
+++ src/test/resources/features/viewSpartan.feature
@@ -2,20 +2,10 @@
 Feature: View all spartans
   @Happy @Status
   Scenario: Querying a Response code with view all spartans
-    When I make a GET request to all
+    When I make a GET request to spartans
     Then I get back a 200 response code
 
   @Happy @View
   Scenario: I make a valid request to view all spartans, I receive a JSON response with all the spartans in the database
-    When I make a GET request to all
+    When I make a GET request to spartans
     Then receive a list of all spartans in a json format
-
-  @Sad @Status
-  Scenario: Querying a Response code with an invalid request to view all spartans
-    When I make a GET request to al
-    Then I get back a 400 response code
-
-  @Sad @Error
-  Scenario: I make an invalid request to view all spartans, receive an error message
-    When I make a GET request to al
-    Then I get back an error message
